c5ab028bbbd76c86c4bf4c6eddbe1bb93d9d2f49
sysecho <707406343@qq.com>
2018-10-09 12:33:34 +0800

    增加分页

--- pages/index/confessionList.js
+++ pages/index/confessionList.js
@@ -5,38 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    confessions: []
+    page:1,
+    size:5,
+    confessions: [],
+    hasmoreData: true,
+    hiddenloading: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var self = this;
-    wx.request({
-      url: 'https://www.mmptech.xin/confession/index/listConfessions',
-      data: {
-        page:1,
-        size:5
-      },
-      method: 'GET',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' // 默认值
-      },
-      success(res) {
-        if(res.data.success){
-          var rows = res.data.rows;          
-          self.setData({ confessions: rows});
-        }else{
-          wx.showToast({
-            title: '暂时没有数据哦~',
-            icon: 'none',
-            mask: true,
-            duration: 20000
-          });
-        }
-      }
-    })
+    this.getConfessionList();
   },
 
   /**
@@ -71,14 +51,19 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.setData({ page: 1, size: 5, confessions: [], hasmoreData: true, hiddenloading: true})
+    this.getConfessionList();
+    console.log('刷新数据')
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('加载更多')
+    this.setData({ hiddenloading: false })
+    this.getConfessionList();
   },
 
   /**
@@ -86,5 +71,44 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getConfessionList: function(){
+    var self = this;
+    wx.showLoading({
+      title: '加载中',
+      icon: 'loading',
+    });
+    
+    wx.request({
+      url: 'https://www.mmptech.xin/confession/index/listConfessions',
+      data: {
+        page: self.data.page,
+        size: self.data.size
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success(res) {
+        wx.hideLoading();
+        if (res.data.success) {
+          var rows = res.data.rows;
+          if (rows.total == 0) that.setData({hasmoreData: false });
+          for (var i = self.data.confessions.length; i < rows.length + self.data.confessions.length; i++) {
+            self.data.confessions.push(rows[i])
+          }
+          
+          self.setData({ page: self.data.page + 1, size: 5, confessions: rows, hiddenloading: true});
+          if (!self.data.hasmoreData) that.setData({ hiddenloading: true });
+        } else {
+          wx.showToast({
+            title: '暂时没有数据哦~',
+            icon: 'none',
+            mask: true,
+            duration: 20000
+          });
+        }
+      }
+    })
   }
 })
