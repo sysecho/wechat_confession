74e8ce0c841862692c7a44e490fc0aff4e9bca2d
鲜小飞 <xianxiaofei@369exhibition.com>
2019-07-09 13:57:01 +0800

    update version.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //app.js
 var gio = require("utils/gio-minp.js").default;
 // version 是你的小程序的版本号, 发版时请调整
-gio('init', 'be0c5825004f8816', 'wx0a2a480aa0822947', { version: '20190326' });
+gio('init', 'be0c5825004f8816', 'wx0a2a480aa0822947', { version: '20190709' });
 
 App({
   onLaunch: function () {
